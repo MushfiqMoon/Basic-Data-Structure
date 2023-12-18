28fe4386b8905d61a21dda73119c0c6be87c41e6
MushfiqMoon <mushfiq.moon013@google.com>
2023-12-18 23:42:47 +0600

    all-question done

---
+++ 14.5 Practice Day 01/problem_four.cpp
@@ -0,0 +1,10 @@
+/*
+Question: Take a queue of size N as input. You need to copy those elements in another queue in reverse order. You might use stack here. You should use STL to solve this problem. After copying in another queue, print the elements of that queue.
+
+Sample Input
+5
+10 20 30 40 50
+Sample Output
+50 40 30 20 10
+
+*/
---
+++ 14.5 Practice Day 01/problem_one.cpp
@@ -0,0 +1,27 @@
+/*
+Question: Take two stacks of size N and M as input and check if both of them are the same or not. Don’t use STL to solve this problem.
+
+Sample Input
+5
+10 20 30 40 50
+5
+10 20 30 40 50
+Sample Output
+YES
+
+Sample Input
+5
+10 20 30 40 50
+4
+10 20 30 40
+Sample Output
+NO
+
+Sample Input
+5
+10 20 30 40 50
+5
+50 40 30 20 10
+Sample Output
+NO
+*/
